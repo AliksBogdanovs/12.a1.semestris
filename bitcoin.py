ffc5b0d7084f1813f97c0f5db7d1968b42032ba7
AliksBogdanovs <90691932+AliksBogdanovs@users.noreply.github.com>
2023-11-16 15:30:38 +0200

    Update bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -1,9 +1,9 @@
 import requests
-
+import json
 URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 bitcoin = requests.get(URL)
-
+print(json.dumps(bitcoin.json(),indent=2))
 cena = bitcoin.json()['bpi']['USD']['rate_float']
 
 input = float(input('ievadi cik gribi nopirkt: '))
